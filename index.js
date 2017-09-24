e8abe4c8674c95494b27892d9deae50d95421cb2
emilymarro <emily.marro@gmail.com>
2017-09-24 02:59:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function sayHiToGrandma(string){
     if (string === string.toLowerCase()){
       return 'I can\'t hear you!';
     }
-    if (string === whisper ){
+    if (string === string.toUpperCase()){
       return 'YES INDEED!';
     }
     if (string === 'I love you, Grandma.'){
