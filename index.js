d4af4b532e7720b06b804c80deb2bcc4501759f8
emilymarro <emily.marro@gmail.com>
2017-09-24 02:23:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -37,23 +37,13 @@ sayHiToGrandma(string) {
   if (string="I love you, Grandma.") {
     console.log("I love you, too.");
   }
-  else if (string.toLowerCase() === lowercase) {
+  //else if (string.toLowerCase() === lowercase) {
+  else if (string="hello") {
     console.log("I can/'t hear you!");
   }
-  else if (string.toUpperCase() === uppercase) {
+  else if (string="HELLO") {
+  //else if (string.toUpperCase() === uppercase) {
     console.log("YES INDEED!");
   }
 }
-describe('sayHiToGrandma(string)', function() {
-  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
-  })
 
-  it('returns "YES INDEED!" if `string` is uppercase', function() {
-    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
-  })
-
-  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
-    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
-  })
-})
