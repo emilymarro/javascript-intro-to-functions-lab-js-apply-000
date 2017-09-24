b3ebc2874d63cfff85b4621ba8770e24435a92e9
emilymarro <emily.marro@gmail.com>
2017-09-24 02:44:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,52 +1,13 @@
+var shout = string.toLowerCase;
+var whisper = string.toUpperCase;
 
-function shout(string){
-  return string.toUpperCase()
-}
-
-function whisper(string){
-  return string.toLowerCase()
-}
-/*describe('logShout(string)', function() {
-  it('calls console.log() its one argument in all caps', function() {
-    const spy = expect.spyOn(console, 'log').andCallThrough()
-
-    logShout('hello')
-
-    expect(spy).toHaveBeenCalledWith('HELLO')
-
-    console.log.restore()
-  })
-})*/
-
-function logWhisper(string){
-  console.log (string.toLowerCase)
-}
-
-function logShout(string){
-  console.log (string.toUpperCase)
-}
-
-/* describe('logWhisper(string)', function() {
-  it('calls console.log() its one argument in all lowercase', function() {
-    const spy = expect.spyOn(console, 'log').andCallThrough()
-
-    logWhisper('HELLO')
-
-    expect(spy).toHaveBeenCalledWith('hello')
-
-    console.log.restore()
-  })
-}) */
-
-sayHiToGrandma(string)
-  if (string === "I love you, Grandma."){
-    console.log("I love you, too.");
-  }
-  //else if (string.toLowerCase() === lowercase) {
-  else if (string === "hello") {
-    console.log("I can/'t hear you!");
-  }
-  else if (string === "HELLO") {
-  //else if (string.toUpperCase() === uppercase) {
-    console.log("YES INDEED!");
+function sayHiToGrandma(string){
+    if (string === shout()){
+      return 'I can\'t hear you!';
+    }
+    if (string === whisper ){
+      return 'YES INDEED!';
+    }
+    if (string === 'I love you, Grandma.'){
+      return 'I love you, too.';
   }
